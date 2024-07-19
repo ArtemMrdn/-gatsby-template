64dc67c691af4658964b339b0a714e09cb677087
ArtemMrdn <artemmrdn47@gmail.com>
2024-07-19 15:16:28 +0300

    init commit

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+import Layout from "../components/Layout";
+import * as styles from "../styles/home.module.css";
+import { Link } from "gatsby";
+
+export default function Home() {
+  return (
+    <Layout>
+      <section className={styles.header}>
+        <div>
+          <h2>Design</h2>
+          <h3>Develop & Deploy</h3>
+          <p>UX designer & web developer</p>
+          <Link to="/projects" className={styles.btn}>
+            My Portfolio Projects
+          </Link>
+        </div>
+      </section>
+    </Layout>
+  );
+}
