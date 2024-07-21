69b9cf34f1f0c895163d7ccef2b8ceeacf810d65
ArtemMrdn <artommrdn47@gmail.com>
2024-07-21 18:10:32 +0300

    Site Metadata & Page Queries

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,4 +9,9 @@
  */
 module.exports = {
   plugins: [],
-}
+  siteMetadata: {
+    title: "Gatsby template",
+    description: "Template with gatsby",
+    copyright: "This website is copyright 2024",
+  },
+};
