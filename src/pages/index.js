596bee33b466a74c45eeb34d9ee511c246107536
ArtemMrdn <artommrdn47@gmail.com>
2024-07-22 16:40:39 +0300

    Optimized Images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,36 @@
-import * as React from "react";
+import React from "react";
+import { Link, graphql } from "gatsby";
 import Layout from "../components/Layout";
 import * as styles from "../styles/home.module.css";
-import { Link } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
-export default function Home() {
+export default function Home({ data }) {
+  console.log(data);
+  const image = getImage(data.file.childImageSharp.gatsbyImageData);
+
   return (
     <Layout>
       <section className={styles.header}>
         <div>
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
-          <p>UX designer & web developer</p>
-          <Link to="/projects" className={styles.btn}>
+          <p>UX designer & web developer based in Manchester.</p>
+          <Link className={styles.btn} to="/projects">
             My Portfolio Projects
           </Link>
         </div>
-        <img src="/banner.png" alt="site banner" style={{ maxWidth: "100%" }} />
+        <GatsbyImage image={image} alt="Banner Image" />
       </section>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query Banner {
+    file(relativePath: { eq: "banner.png" }) {
+      childImageSharp {
+        gatsbyImageData(layout: FULL_WIDTH)
+      }
+    }
+  }
+`;
